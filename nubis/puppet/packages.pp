aa17a0d40e3b0792668e8c01e8156f7367885230
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-02-27 10:47:03 -0500

    add the automated installer package too

--- nubis/puppet/packages.pp
+++ nubis/puppet/packages.pp
@@ -2,6 +2,7 @@ $vsql_major_version = '8.1'
 $vsql_version = "${vsql_major_version}.1-13"
 
 $tableau_version = '10-5-1'
+$tableau_installer_version = '10-5-0'
 
 # Vertica SQL
 package { 'vsql':
@@ -39,6 +40,12 @@ package { [
   install_options => [
     '--nopre',
   ],
+}
+-> package { 'tableau-installer':
+  ensure          => present,
+  provider        => 'rpm',
+  name            => 'tableau-server-automated-installer',
+  source          => "https://raw.githubusercontent.com/tableau/server-install-script-samples/master/linux/automated-installer/packages/tableau-server-automated-installer-${$tableau_installer_version}.noarch.rpm",
 }
 
 
