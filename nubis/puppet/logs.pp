1c920f42c3dcd0bd0036baee996ea58684af0bb6
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-04-23 13:02:27 -0400

    ship tabsvc logs off to fluentd

---
+++ nubis/puppet/logs.pp
@@ -0,0 +1,15 @@
+fluentd::configfile { $project_name: }
+
+fluentd::source { 'tabsvc':
+  configfile => $project_name,
+  type       => 'tail',
+  format     => 'none',
+
+  tag        => "forward.${project_name}.tabsvc",
+  config     => {
+    'read_from_head' => true,
+    'path_key'       => 'tailed_path',
+    'path'           => '/var/opt/tableau/tableau_server/data/tabsvc/logs/*/*.log',
+    'pos_file'       => "/var/log/${project_name}.pos",
+  },
+}
