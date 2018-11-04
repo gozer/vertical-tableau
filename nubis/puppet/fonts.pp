ce7978101a07dae66604d5addebcffe31762f54f
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-11-04 14:53:50 -0500

    fix missing directory

--- nubis/puppet/fonts.pp
+++ nubis/puppet/fonts.pp
@@ -1,3 +1,9 @@
+file { '/usr/share/fonts':
+  ensure  => directory,
+  mode    => '0755',
+  owner   => 'root',
+  group   => 'root',
+}
 file { '/usr/share/fonts/tableau':
   ensure  => directory,
   recurse => true,
