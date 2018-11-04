37933569193cfc2940c34f7c81abb2cc91237964
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-11-04 13:49:33 -0500

    Install Benton fonts

---
+++ nubis/puppet/fonts.pp
@@ -0,0 +1,12 @@
+file { '/usr/share/fonts/tableau':
+  ensure  => directory,
+  recurse => true,
+  purge   => false,
+  owner   => 'root',
+  group   => 'root',
+  source  => 'puppet:///nubis/files/fonts',
+}
+  -> -> exec { 'Update font cache':
+  command => 'fc-cache -v /usr/share/fonts',
+  path    => ['/bin','/sbin','/usr/bin','/usr/sbin','/usr/local/bin','/usr/local/sbin'],
+}
